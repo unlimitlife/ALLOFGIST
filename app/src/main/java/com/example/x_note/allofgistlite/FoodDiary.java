baea13f79ac03385e2d3315fa9dd84c3d7b0f381
leedonggun <unlimitlife@naver.com>
2019-02-13 11:07:39 +0900

    Food_diary image url extraction(can't save img only from gist board)

--- app/src/main/java/com/example/x_note/allofgistlite/FoodDiary.java
+++ app/src/main/java/com/example/x_note/allofgistlite/FoodDiary.java
@@ -2,6 +2,8 @@ package com.example.x_note.allofgistlite;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -9,12 +11,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -28,6 +34,8 @@ public class FoodDiary extends AppCompatActivity {
     private LinearLayout secondFloor;
     private LinearLayout secondRestaurant;
 
+    ImageView foodDiaryImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,17 +45,19 @@ public class FoodDiary extends AppCompatActivity {
         secondFloor = (LinearLayout) findViewById(R.id.second_floor);
         secondRestaurant = (LinearLayout) findViewById(R.id.second_restaurant);
 
+        foodDiaryImage = (ImageView)findViewById(R.id.image_food_diary);
+
         firstFloor.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                startMyTask(new FirstRestaurantTask(),"https://www.gist.ac.kr/kr/html/sub05/050601.html");
+                startMyTask(new FirstRestaurantSaveTask(),"https://www.gist.ac.kr/kr/html/sub05/050601.html");
             }
         });
 
         secondFloor.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                startMyTask(new FirstRestaurantTask(),"https://www.gist.ac.kr/kr/html/sub05/050603.html");
+                startMyTask(new FirstRestaurantSaveTask(),"https://www.gist.ac.kr/kr/html/sub05/050603.html");
             }
         });
 
@@ -229,6 +239,96 @@ public class FoodDiary extends AppCompatActivity {
         }
     }
 
+    //------------------------------------------------------------------
+    //1학 학식 사진 저장
+    public class FirstRestaurantSaveTask extends AsyncTask<String, Void, Bitmap> {
+
+        Bitmap mBitmap;
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            String Address = params[0];
+            URL url;
+            BufferedReader br;
+            HttpURLConnection conn;
+            String protocol = "GET";
+            try {
+                url = new URL(Address);
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod(protocol);
+                br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                String line;
+                boolean findFirstLine = false;
+                while ((line = br.readLine()) != null) {
+                    if(findFirstLine){
+                        line = line.replace("\t\t\t\t\t\t\t\t\t\t<a href='","");
+                        line = line.replace("'>","");
+                        line = line.replaceAll("amp;","");
+                        break;
+                    }
+                    if (line.startsWith("\t\t\t\t\t\t\t\t\t<div class='bd_item_box'>")) {
+                        findFirstLine =  true;
+                    }
+                }
+                if (!line.isEmpty()) {
+                    String boardAddress = Address + line;
+
+                    url = new URL(boardAddress);
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod(protocol);
+                    br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+                    line = "";
+                    int i=0;
+                    while ((line = br.readLine()) != null) {
+                        i++;
+                        if(line.startsWith("\t\t\t<li><a href='")){
+                            line = line.replaceFirst("\t\t\t<li><a href='","");
+                            line = line.split("' title='")[0];
+                            line = line.replaceAll("amp;","");
+                            break;
+                        }
+                        if(line.contains("\"external_image\"")){
+                            line = line.split("\"external_image\"")[2];
+                            line = line.replace(" src=\"","");
+                            line = line.replace("\"></p></p>","");
+                            break;
+                        }
+                    }
+                    if(!line.isEmpty()){
+                        if(line.contains("gist.ac.kr"))
+                            Address = line;
+                        else
+                            Address += line;
+                    }
+
+                    url = new URL("http://www.gist.ac.kr/_prog/download/?editor_image=20190207131423369_J8ACRXX1.png");
+                    conn = (HttpURLConnection) url.openConnection();
+
+                    conn.setDoInput(true);
+                    conn.connect();
+                    BufferedInputStream input = new BufferedInputStream(conn.getInputStream());
+
+                    mBitmap = BitmapFactory.decodeStream(input);
+                    input.close();
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return mBitmap;
+        }
+
+
+
+        @Override
+        protected void onPostExecute(Bitmap mBitmap) {
+            if(mBitmap != null)
+                foodDiaryImage.setImageBitmap(mBitmap);
+        }
+    }
+
     //asynctask 병렬처리
     public void startMyTask(AsyncTask asyncTask, String... params){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
@@ -236,5 +336,4 @@ public class FoodDiary extends AppCompatActivity {
         else
             asyncTask.execute(params);
     }
-
 }
